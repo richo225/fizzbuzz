fd5452a5220c82c04e4a0a3cc92119c1ef0f1417
richo225 <richo225@gmail.com>
2016-07-04 00:23:42 +0100

    Test for multiples of 3 -> fizz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,15 @@
 require 'fizzbuzz'
 
+#simplest code to pass first test
 describe 'fizzbuzz' do  #describe the method
   it 'returns "fizz" when passed 3' do  #example in an it block
     expect(fizzbuzz(3)).to eq 'fizz' #test code -> expectation
+  end
+end
+
+#another test for multiples of 3
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
   end
 end
