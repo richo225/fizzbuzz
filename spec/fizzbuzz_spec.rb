e8665155c5a4f9292f433a86a6f0c290ecbce924
richo225 <richo225@gmail.com>
2016-07-04 00:15:32 +0100

    Add fizzbuzz.rb with simplest code to pass test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,3 +1,5 @@
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do  #describe the method
   it 'returns "fizz" when passed 3' do  #example in an it block
     expect(fizzbuzz(3)).to eq 'fizz' #test code -> expectation
