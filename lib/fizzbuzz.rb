0668fc34386223241b3e851a80df24ba5bbd68f8
richo225 <richo225@gmail.com>
2016-07-04 17:46:04 +0100

    Completed round 9

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,1 +1,8 @@
-
+def fizzbuzz number
+  case
+  when number % 15 == 0 then "fizzbuzz"
+  when number % 3 == 0 then "fizz"
+  when number % 5 == 0 then "buzz"
+  else number
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,25 @@
+require "fizzbuzz.rb"
 
+describe "fizzbuzz" do
+  it "returns fizz when passed 9" do
+    expect(fizzbuzz(9)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns buzz when passed 10" do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns fizzbuzz when passed 15" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 16 when passed 16" do
+    expect(fizzbuzz(16)).to eq 16
+  end
+end
