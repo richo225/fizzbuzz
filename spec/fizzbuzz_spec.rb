1a607bb075a4f71bd8330c150a54ab2549df9067
richo225 <richo225@gmail.com>
2016-07-04 16:25:34 +0100

    Completed round 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,31 @@
+require "fizzbuzz.rb"
 
+describe "fizzbuzz" do
+  it "returns 'fizz' when passed 3" do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'fizz' when passed 33" do
+    expect(fizzbuzz(33)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'buzz' when passed 50" do
+    expect(fizzbuzz(50)).to eq "buzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'fizzbuzz' when passed 30" do
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 11 when passed 11" do
+    expect(fizzbuzz(11)).to eq 11
+  end
+end
